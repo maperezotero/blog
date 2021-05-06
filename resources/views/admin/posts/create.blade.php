efab467cadd11c7e58672a2fbf74951b3d2e8b6c
maperezotero <maperezotero@gmail.com>
2021-05-06 20:36:06 +0200

    Formulario creacion posts OK

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,7 +3,68 @@
 @section('title', 'SuperBit')
 
 @section('content_header')
-    <h1>Crear nuevo post</h1>
+    
+	<div class="card">
+		<div class="card-body">
+			{!! Form::open(['route' => 'admin.posts.store', 'autocomplete' => 'off']) !!}
+
+				<div class="form-group">
+					{!! Form::label('name', 'Nombre:') !!}
+					{!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Introduzca el nombre del post']) !!}
+				</div>
+
+				<div class="form-group">
+					{!! Form::label('slug', 'Slug:') !!}
+					{!! Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'Introduzca el slug del post', 'readonly']) !!}
+				</div>
+
+				<div class="form-group">
+					{!! Form::label('category_id', 'Categoría') !!}
+					{!! Form::select('category_id', $categories, null, ['class' => 'form-control']) !!}
+				</div>
+
+				<div class="form-group">
+					<p class="font-weight-bold">Etiquetas</p>
+					@foreach ($tags as $tag)
+						
+						<label class="mr-2">
+							{!! Form::checkbox('tags[]', $tag->id, null) !!}
+							{{ $tag->name }}
+						</label>
+
+					@endforeach
+				</div>
+
+				<div class="form-group">
+					<p class="font-weight-bold">Estado</p>
+
+					<label>
+						{!! Form::radio('status', 1, true) !!}
+						Borrador
+					</label>
+
+					<label>
+						{!! Form::radio('status', 1) !!}
+						Publicado
+					</label>
+				</div>
+
+				<div class="form-group">
+					{!! Form::label('extract', 'Extracto') !!}
+					{!! Form::textarea('extract', null, ['class' => 'form-control']) !!}
+				</div>
+
+				<div class="form-group">
+					{!! Form::label('body', 'Cuerpo del post') !!}
+					{!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+				</div>
+
+				{!! Form::submit('Crear post', ['class' => 'btn btn-primary']) !!}
+
+			{!! Form::close() !!}
+		</div>
+	</div>
+
 @stop
 
 @section('content')
@@ -15,5 +76,30 @@
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
-@stop
+	
+	<script src="{{ asset('vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js') }}"></script>
+	<script src="https://cdn.ckeditor.com/ckeditor5/27.1.0/classic/ckeditor.js"></script>
+	
+	<script>
+		$(document).ready( function() {
+			$("#name").stringToSlug({
+				setEvents: 'keyup keydown blur',
+				getPut: '#slug',
+				space: '-'
+			});
+		});
+
+		ClassicEditor
+        .create( document.querySelector( '#extract' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+
+		ClassicEditor
+        .create( document.querySelector( '#body' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+	</script>
+
+@endsection
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,6 +3,9 @@
 @section('title', 'SuperBit')
 
 @section('content_header')
+
+	<a class="btn btn-secondary btn-sm float-right" href="{{ route('admin.posts.create') }}">Nuevo post</a>
+
     <h1>Listado de posts</h1>
 @stop
 
